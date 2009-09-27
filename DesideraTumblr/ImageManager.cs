ea749ac36484b5fe2250e314ed842c205fda389b
Yuki Mitsui <silphire@gmail.com>
2009-09-27 14:03:35 +0900

    created new class "ImageManager"

---
+++ DesideraTumblr/ImageManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesideraTumblr
+{
+    /// <summary>
+    /// 読み込んだ画像・キャッシュ中の画像・表示中の画像などを管理するクラス
+    /// </summary>
+    class ImageManager
+    {
+    }
+}
