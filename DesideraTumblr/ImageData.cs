f362216103f47d53f69f0d8502c37431b1880529
Yuki Mitsui <silphire@gmail.com>
2009-09-29 21:24:48 +0900

    created ImageData

---
+++ DesideraTumblr/ImageData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesideraTumblr
+{
+    public class ImageData
+    {
+        Tumblr.Photo photo;
+        string path;
+        System.Drawing.Image original;
+        System.Drawing.Image thumbnail;
+
+        ImageData(string id)
+        {
+        }
+
+        void Load(string id)
+        {
+        }
+    }
+}
--- DesideraTumblr/ImageManager.cs
+++ DesideraTumblr/ImageManager.cs
@@ -8,7 +8,17 @@ namespace DesideraTumblr
     /// <summary>
     /// 読み込んだ画像・キャッシュ中の画像・表示中の画像などを管理するクラス
     /// </summary>
-    class ImageManager
+    public class ImageManager
     {
+        private List<ImageData> CachedImages;
+
+        ImageManager()
+        {
+            CachedImages = new List<ImageData>();
+        }
+
+        public void GetImage(int index, int num)
+        {
+        }
     }
 }
